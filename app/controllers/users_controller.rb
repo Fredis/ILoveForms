f782e9a2778a6208cd9e615928acc6205876fed6
Fredis <fredericstojicevic@gmail.com>
2018-10-30 18:34:15 +0100

    Add some style corrections.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,14 +1,15 @@
 class UsersController < ApplicationController
+  
   def new
     @user = User.new
   end
 
-  #def create
-  #	User.create!(username:params[:username], email:params[:email], bio:params[:bio])
-  #end
-
   def create
-    User.create!(username: params[:user][:username], email: params[:user][:email], bio: params[:user][:bio])
+  	if params[:user]
+    	User.create!(username: params[:user][:username], email: params[:user][:email], bio: params[:user][:bio])
+    else
+    	User.create!(username:params[:username], email:params[:email], bio:params[:bio])
+    end 
   end
 
 end
